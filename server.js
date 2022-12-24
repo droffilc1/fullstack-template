671612fa49fa7e4b6cd8307514054e7fe58d1048
Clifford Mapesa <cliffordmapesa8@gmail.com>
2022-12-24 16:55:36 +0300

    Add port and  Mongodb connection

--- server.js
+++ server.js
@@ -5,13 +5,28 @@ const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
 let db,
-  dbConnectionString = process.env.DB_STRING,
+  uri = process.env.DB_STRING,
   dbName = 'sample_mflix',
   collection
 
-MongoClient.connect(dbConnectionString)
-  .then(client => {
-    console.log('Connected to Database');
-    db = client.db(dbName)
-    collection = db.collection('comments')
-  })
+async function main() {  
+  try {
+    // Connect to Mongodb cluster
+    MongoClient.connect(uri)
+      .then(client => {
+        console.log('Connected to Database');
+        db = client.db(dbName)
+        collection = db.collection('movies')      
+    })
+        
+  } catch (e) {
+    console.log(e);
+  } 
+}
+
+main().catch(console.error)
+
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+})
