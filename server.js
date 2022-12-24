79ecc3f968b3ddf0997f1250ec37250491455cc2
Clifford Mapesa <cliffordmapesa8@gmail.com>
2022-12-24 18:03:31 +0300

    Add endpoint in server.js

--- server.js
+++ server.js
@@ -34,7 +34,17 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors())
+app.use(cors)
+
+
+app.get('/', async (req, res) => {
+  try {
+    res.render('index.ejs')
+  } catch (error) {
+    res.status(404).send({ message: error.message})
+  }
+  
+})
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
